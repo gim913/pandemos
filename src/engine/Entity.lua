3790b510fd5c70d610d9f36532f365782d716bef
GiM <gim913@gmail.com>
2019-06-23 21:56:57 +0200

    unocuppy in entity

--- src/engine/Entity.lua
+++ src/engine/Entity.lua
@@ -1,7 +1,9 @@
+-- imported modules
 local class = require 'engine.oop'
 local entities = require 'engine.entities'
 local map = require 'engine.map'
 
+-- class
 Entity = class('Entity')
 
 function Entity:ctor(initPos)
@@ -18,8 +20,13 @@ function Entity:onAdd()
 end
 
 function Entity:occupy()
-	local idx = self.pos.y*map.width() + self.pos.x
+	local idx = self.pos.y * map.width() + self.pos.x
 	entities.occupy(idx, self.id)
+end
+
+function Entity:unoccupy()
+	local idx = self.pos.y * map.width() + self.pos.x
+	entities.unoccupy(idx, self.id)
 end
 
 return Entity
